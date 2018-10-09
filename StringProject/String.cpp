c902a382643a0ef8d9b76538e4025f10f45d7ab8
Rakiiii <sudaru2@gmail.com>
2018-10-09 19:35:37 +0300

    Add =overload add+overload add string print out add some bugs projects can be compiled right now add some todo's add debug start refactored preveous lines

--- StringProject/String.cpp
+++ StringProject/String.cpp
@@ -1,15 +1,13 @@
 ﻿#include "String.h"
-#include <string.h>
+#include "pch.h"
 using namespace std;
 
-//TODO : написать конструктор копированния
-//TODO : написать конструктор чистой строки
-//TODO : написать конструктор новой строки
-//TODO : написать getter для длины и строки
-//TODO : написать перегрузку конкатенации строки
+
+
+//TODO : написать деструктор
 //TODO : написать перегрузку операции сравнения
 //TODO : написать преобразование в числа разных видов
-//TODO : написать форматный вывод строки
+
 
 
 	//getter для строки
@@ -31,6 +29,8 @@ using namespace std;
 	String::String()
 	{
 		this->length;
+		this->_string = new char[1];
+		this->_string = 0;
 	}
 
 	//конструктор копированния
@@ -47,29 +47,61 @@ using namespace std;
 
 		//копируем все символы
 
-		this->copyChar(secondString._string);
+		strcpy(this->_string,secondString._string);
 
+		this->_string[this->length + 1] = '\0';
 	}
 
 	//копированния строки
-
-	void String::copyChar(const char *copyedString)
-	{
-		//в цикле копируем все символы 
-		for (int i = 0; i < this->length - 1 ; i++)
-		{
-			this->_string[i] = copyedString[i];
-		}
-		//добавляем закрывающий ноль
-		this->_string[this->length] = '\0';
-	}
 
 	//конструктор новой строки
 	String::String(const char *newString)
 	{
 		this->length = strlen(newString);
 		this->_string = new char[this->length + 1];
-		this->copyChar(newString);
+		strcpy(this->_string, newString);
+		this->_string[this->length + 1] = '\0';
+	}
+
+	//перегрузка оператора конкатенации
+	String& String::operator+(const String& rightString)
+	{
+		//создаем результируюшую строку
+		String newString;
+		//освобождаем память
+		delete newString._string;
+		//получаем длину новой строки
+		newString.length = this->length + rightString.length;
+		//выделяем память строки
+		newString._string = new char[newString.length + 1];
+		//складываем непосредственно строки
+		strcpy(newString._string,this->_string);
+		strcpy(newString._string + this->length, rightString._string);
+		newString._string[newString.length + 1] = '\0';
+		//возвращаем наш объект
+		return newString;
+	}
+
+	//перегрузка оператора присваивания
+
+	String& String::operator= (const String& rightString)
+	{
+		if (this != &rightString)
+		{
+			delete this->_string;
+			this->length = rightString.length;
+			this->_string = new char[this->length + 1];
+			strcpy(this->_string, rightString._string);
+			this->_string[this->length + 1] = '\0';
+		}
+		return *this;
+	}
+
+	//метод вывода строки
+
+	void String::printString()
+	{
+		cout << this->_string << endl;
 	}
 
 
